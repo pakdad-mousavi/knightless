80b4bb888b585c7f563d9adce8a7a51a13c7bc77
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-12 15:16:16 +0800

    Create routes for hall of fame page and its players.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import * as homeController from '../controllers/home.js';
+import * as playersController from '../controllers/players.js';
 
 const router = express.Router();
 
 router.route('/').get(homeController.index);
+router.route('/hall-of-fame').get(playersController.getPlayers);
+router.route('/hall-of-fame/:id').get(playersController.getPlayer);
 
 export default router;
