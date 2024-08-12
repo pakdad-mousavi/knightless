bd8b064ddfcd955f0e1e07c5250fa4c22a677398
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-12 23:54:23 +0800

    Fix the page title given by the controller.

--- controllers/players.js
+++ controllers/players.js
@@ -7,7 +7,7 @@ export const getPlayers = (req, res) => {
 export const getPlayer = async (req, res) => {
   const { id } = req.params;
   const player = await Player.findOne({ id }).lean();
-  const fullname = player.personalDetails.firstName + player.personalDetails.lastName;
+  const fullname = player.personalDetails.firstName + ' ' + player.personalDetails.lastName;
   const dto = {
     player,
     title: fullname,
