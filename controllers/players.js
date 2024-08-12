20fff0eb3b2b1599984d6a43d8a879961229e6c8
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-12 15:16:38 +0800

    Create players controller for hall of fame pages.

---
+++ controllers/players.js
@@ -0,0 +1,12 @@
+import { Player } from '../models/player.js';
+
+export const getPlayers = (req, res) => {
+  res.send('Hall of fame stuff goes here. :)');
+};
+
+export const getPlayer = async (req, res) => {
+  const { id } = req.params;
+  const player = await Player.findOne({ id }).lean();
+  const fullname = player.personalDetails.firstName + player.personalDetails.lastName;
+  res.render('hall-of-fame/player', {player, title: fullname });
+};
