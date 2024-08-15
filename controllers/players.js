755d16731f96fe7068c548a1bf6a1426a519ea4c
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-15 14:41:00 +0800

    Get filter results from database and send to client to render.

--- controllers/players.js
+++ controllers/players.js
@@ -1,11 +1,74 @@
 import { Player } from '../models/player.js';
 import { getPlayingStyles, getDistinctPlayingStyles } from '../helpers/getPlayingStyles.js';
 
-export const getPlayers = (req, res) => {
-  const playingStyles = getDistinctPlayingStyles();
+export const getPlayers = async (req, res) => {
+  const cookieFilters = req.signedCookies?.filters;
+
+  const playingStyles = cookieFilters?.playingStyles || [];
+  let ratingA = cookieFilters?.ratingA || false;
+  let ratingB = cookieFilters?.ratingB || false;
+  const hasBeenChampion = cookieFilters?.hasBeenChampion === true;
+
+  const filter = {};
+  if (playingStyles.length) {
+    filter['playingStyle'] = { $in: playingStyles };
+  }
+  if (ratingA && ratingB) {
+    // Make sure ratingA is always lower than ratingB
+    if (ratingA > ratingB) {
+      const temp = ratingA;
+      ratingA = ratingB;
+      ratingB = temp;
+    }
+    filter['personalDetails.peakFideRating'] = { $gte: ratingA, $lte: ratingB };
+  }
+  if (hasBeenChampion) {
+    filter['personalDetails.hasBeenChampion'] = true;
+  }
+
+  const options = {
+    playingStyles: {},
+    ratingA: ratingA || 2500,
+    ratingB: ratingB || 2900,
+    hasBeenChampion,
+  };
+
+  const distinctPlayingStyles = getDistinctPlayingStyles();
+
+  distinctPlayingStyles.forEach((style) => (options.playingStyles[style] = playingStyles.includes(style)));
+
+  const totalRecords = (await Player.find(filter).lean()).length;
+
+  const { page } = req.query;
+  const pageNumber = Number(page);
+
+  // Paging object:
+  const paging = {
+    currentPage: 1,
+    pageSize: 9,
+    hasNext: false,
+    hasPrev: false,
+    totalRecords,
+  };
+
+  if (pageNumber && Number.isInteger(pageNumber) && pageNumber > 0) {
+    // Validate pageNumber
+    paging.currentPage = pageNumber; // Update the current page
+    paging.hasPrev = pageNumber <= 1 ? false : true; // Update hasPrev based on the current page
+  } else {
+    paging.currentPage = 1; // If pageNumber is invalid, default it to 1
+  }
+
+  // Check to see if there is a next page from the data fetched
+  const skip = (paging.currentPage - 1) * paging.pageSize;
+  paging.hasNext = totalRecords - skip > paging.pageSize ? true : false;
+
+  const players = await Player.find(filter).skip(skip).limit(paging.pageSize).lean();
 
   const model = {
-    playingStyles,
+    players,
+    options,
+    paging,
     title: 'Hall Of Fame',
     isHomePage: false,
   };
@@ -16,18 +79,23 @@ export const getPlayers = (req, res) => {
 export const applySearchFilters = (req, res) => {
   const { playingStyles: styles, ratingA: rtA, ratingB: rtB, formerChampion: chmp } = req.body;
 
-  if (!Number.isInteger(rtA) || !Number.isInteger(rtB)) {
-    res.send('Ratings need to be numbers.')
-  };
-
-    const filters = {
-      playingStyles: styles ? [].concat(styles) : [],
-      ratingsA: rtA,
-      ratingsB: rtB,
-      hasBeenChampion: chmp === 'true',
+  if (!Number.isInteger(Number(rtA)) || !Number.isInteger(Number(rtB))) {
+    res.send('Ratings need to be numbers.');
   }
 
-  
+  const filters = {
+    playingStyles: styles ? [].concat(styles) : [],
+    ratingA: rtA,
+    ratingB: rtB,
+    hasBeenChampion: chmp === 'true',
+  };
+
+  res.cookie('filters', filters, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Lax',
+    signed: true,
+  });
 
   res.redirect('/hall-of-fame');
 };
