2a39118f23ebc7e6d02cc68babfb86c23607866d
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-15 12:16:55 +0800

    Create a post route for the filters.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.route('/').get(homeController.index);
 router.route('/hall-of-fame').get(playersController.getPlayers);
+router.route('/hall-of-fame').post(playersController.applySearchFilters);
 router.route('/hall-of-fame/:id').get(playersController.getPlayerById);
 
 export default router;
