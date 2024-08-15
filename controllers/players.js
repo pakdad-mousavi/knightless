f0afd5ab4030342a55724217ba66d1137d3b5bed
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-15 12:30:51 +0800

    Get and clean the filters from the form.

--- controllers/players.js
+++ controllers/players.js
@@ -1,5 +1,5 @@
 import { Player } from '../models/player.js';
-import { getPlayingStyles, getDistinctPlayingStyles} from '../helpers/getPlayingStyles.js';
+import { getPlayingStyles, getDistinctPlayingStyles } from '../helpers/getPlayingStyles.js';
 
 export const getPlayers = (req, res) => {
   const playingStyles = getDistinctPlayingStyles();
@@ -13,7 +13,24 @@ export const getPlayers = (req, res) => {
   res.render('hall-of-fame/list', model);
 };
 
-export const applySearchFilters = (req, res) => {};
+export const applySearchFilters = (req, res) => {
+  const { playingStyles: styles, ratingA: rtA, ratingB: rtB, formerChampion: chmp } = req.body;
+
+  if (!Number.isInteger(rtA) || !Number.isInteger(rtB)) {
+    res.send('Ratings need to be numbers.')
+  };
+
+    const filters = {
+      playingStyles: styles ? [].concat(styles) : [],
+      ratingsA: rtA,
+      ratingsB: rtB,
+      hasBeenChampion: chmp === 'true',
+  }
+
+  
+
+  res.redirect('/hall-of-fame');
+};
 
 export const getPlayerById = async (req, res) => {
   const { id } = req.params;
