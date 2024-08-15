e9af4818f3f6cf7f41fcea827d07b2d153ff4c8a
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-15 11:54:02 +0800

    Add dynamic rating range display for slider.

---
+++ assets/js/core.js
@@ -0,0 +1,27 @@
+const getsliderValues = (sliders) => {
+  let sliderLeft = Number(sliders[0].value);
+  let sliderRight = Number(sliders[1].value);
+
+  // Slider 1 must always be less than slider 2
+  if (sliderLeft > sliderRight) {
+    let tmp = sliderRight;
+    sliderRight = sliderLeft;
+    sliderLeft = tmp;
+  }
+
+  return { sliderLeft, sliderRight };
+};
+
+const watchSliders = (sliders, displayElement) => {
+  sliders.forEach((slider) => {
+    slider.addEventListener('input', () => {
+      // Get slider values and display them
+      const { sliderLeft, sliderRight } = getsliderValues(sliders);
+      displayElement.innerText = `${sliderLeft} - ${sliderRight}${sliderRight === 2800 ? '+' : ''}`;
+    });
+  });
+};
+
+const sliders = document.querySelectorAll('.slider > input[name=rating-range]');
+const displayElement = document.querySelector('.rating-range-values');
+watchSliders(sliders, displayElement);
