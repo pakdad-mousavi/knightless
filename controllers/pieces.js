f00b2179559a5b20f0503c408cc18e2f752a971a
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-20 13:49:26 +0800

    Create getPieceById controller.

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -1,8 +1,23 @@
-export const getPieceById = (req, res) => {
-  const model = {
-    title: "Fancy Piece",
-    isHomePage: false,
-  }
+import { Piece } from '../models/piece.js';
+import { getOtherPieces } from '../helpers/getOtherPieces.js';
 
-  res.render('pieces/piece', model);
+export const getPieceById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const piece = await Piece.findOne({ id }).lean();
+    const otherPieces = getOtherPieces(piece.name);
+
+    const model = {
+      title: piece.name,
+      piece,
+      otherPieces,
+      isHomePage: false,
+    };
+
+    res.render('pieces/piece', model);
+  } catch (e) {
+    console.log(e);
+    res.send(`Cannot find ${id}`);
+  }
 };
