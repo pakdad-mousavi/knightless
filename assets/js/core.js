0006c993f1b9da5848a2617982c0ff0ac14cb2bd
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-08-30 16:32:57 +0800

    Remove old puzzle board code for core.js.

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,5 +1,6 @@
 import { Chess } from '/chessjs/chess.js';
-import { createGameFen, checkForPieces, highlightSquare, removeAllHighlights, highlightChecks, playMoveAudio } from './chessUtils.mjs';
+import { createGameFen, checkForPieces, highlightSquare } from './chessUtils.mjs';
+import { setUpPuzzleBoard } from './puzzleBoard.mjs';
 
 const getsliderValues = (sliders) => {
   let sliderLeft = Number(sliders[0].value);
@@ -205,158 +206,6 @@ const setUpPositionBoards = (boardElements) => {
   });
 };
 
-const setUpPuzzleBoards = (boardElements) => {
-  if (!boardElements.length) return;
-
-  boardElements.forEach((boardElement) => {
-    // Get the game position and the puzzle solution
-    let pgn = boardElement.dataset.pgn.replaceAll(',', ' ').split(' ');
-    const lastMove = pgn.splice(pgn.length - 1, 1)[0];
-    pgn = pgn.join(' ');
-
-    const solution = boardElement.dataset.solution.split(',');
-
-    // Get the move box and message box elements for the respective board
-    const id = boardElement.id;
-    const pgnPanel = document.querySelector(`.${id}.pgn-panel`);
-    const messageBox = pgnPanel.querySelector(`.message-box`);
-    // const moveBox = pgnPanel.querySelector(`.move-box`);
-
-    // Create the game with the pgn
-    const game = new Chess();
-    game.loadPgn(pgn);
-
-    // Deduce the board orientation based on number of moves done
-    const gameLength = game.moveNumber();
-    const orientation = gameLength % 2 === 0 ? 'white' : 'black';
-
-    // Keep track of the moves used by the player
-    let movesUsed = 0;
-    let puzzleOver = false;
-
-    // Only allow the player to move their respective pieces
-    const onDragStart = (_, piece) => {
-      if ((orientation === 'white' && piece.search(/^w/) === -1) || (orientation === 'black' && piece.search(/^b/) === -1) || puzzleOver) {
-        return false;
-      }
-    };
-
-    const onDrop = (from, to) => {
-      try {
-        // Make the move (checks if it is legal or not)
-        const move = game.move({ from, to });
-
-        const isNonCapture = move.flags.search('e') === -1 && move.flags.search('c') === -1;
-        const isCheck = game.inCheck();
-        if (isCheck) {
-          playMoveAudio('check', 1);
-        } else if (isNonCapture) {
-          playMoveAudio('move', 0.5);
-        } else {
-          playMoveAudio('capture', 0.5);
-        }
-
-        removeAllHighlights(boardElement); // First, remove all current highlights
-        highlightChecks(game, boardElement); // Then, check for a check on the next turns' player's king
-        highlightSquare(boardElement, move.from, 'move-highlight'); // Highlight the "from" move
-        highlightSquare(boardElement, move.to, 'move-highlight'); // Highlight the "to" move
-
-        // Logical game variables
-        let isCorrectMove = move.lan === solution[movesUsed];
-
-        // If the puzzle is over...
-        if ((isCorrectMove && solution.length === movesUsed + 1) || game.isCheckmate()) {
-          updatePgnPanel({ move, isCorrect: true, puzzleOver: true }); // Update pgn panel
-          puzzleOver = true; // Update puzzleOver
-          return;
-        }
-
-        // If the move is correct...
-        if (isCorrectMove) {
-          updatePgnPanel({ move, isCorrect: true, puzzleOver: false }); // Update pgn panel
-          highlightSquare(boardElement, move.from, 'move-highlight'); // Highlight the "from" move
-          highlightSquare(boardElement, move.to, 'move-highlight'); // Highlight the "to" move
-
-          // Make the opponents move
-          window.setTimeout(() => {
-            const opponentMove = game.move(solution[movesUsed + 1]); // Make the next move
-            board.position(game.fen()); // Set the board position
-            removeAllHighlights(boardElement); // Remove highlights
-            highlightChecks(game, boardElement); // Highlight any new checks
-            highlightSquare(boardElement, opponentMove.from, 'move-highlight'); // Highlight the "from" move
-            highlightSquare(boardElement, opponentMove.to, 'move-highlight'); // Highlight the "to" move
-            movesUsed += 2; // Update movesUsed
-          }, 400);
-        }
-
-        // If the move is wrong...
-        else {
-          updatePgnPanel({ move, isCorrect: false, puzzleOver: false });
-          game.undo(); // Undo the move
-
-          window.setTimeout(() => {
-            board.position(game.fen()); // Return back to position before the move
-            removeAllHighlights(boardElement); // Remove any highlights
-            highlightChecks(game, boardElement); // Check for any old checks
-          }, 300);
-        }
-      } catch {
-        playMoveAudio('illegal', 1);
-        return 'snapback';
-      }
-    };
-
-    // Create the chessboard
-    const config = {
-      position: game.fen() || 'start',
-      draggable: true,
-      onDrop,
-      onDragStart,
-      moveSpeed: 150,
-      pieceTheme: '/chesspieces/{piece}.svg',
-    };
-
-    const board = new Chessboard(boardElement, config);
-    board.orientation(orientation);
-
-    const updatePgnPanel = (obj) => {
-      // Initial run, set the message to tell the player who's turn it is
-      if (obj.orientation) {
-        const message = [];
-        message.push(`<h4>It's your turn.</h4>`);
-        message.push(`<span class="font-normal">Find the best move for ${obj.orientation}.</span>`);
-        messageBox.innerHTML = message.join('');
-      }
-
-      if (obj.move) {
-        // Set the message to tell the player the puzzle is over
-        if (obj.puzzleOver && obj.isCorrect) {
-          messageBox.innerHTML = `<h4>Puzzle solved!</h4><span class="font-normal">Great job.</span>`;
-        } else {
-          // Tell the player to keep going or to try again based on move
-          const messageType = obj.isCorrect ? 'success' : 'fail';
-          const messageTitle = obj.isCorrect ? `<h4><span class="${messageType}">${obj.move.san} is the best move.</span></h4>` : `<h4><span class="${messageType}">${obj.move.san} is not the right move.</span></h4>`;
-          const messageText = obj.isCorrect ? `<span class="font-normal">Keep going...</span>` : `<span class="font-normal">Try again.</span>`;
-          messageBox.innerHTML = messageTitle + messageText;
-        }
-      }
-    };
-
-    // Give the pgn panel its initial message
-    updatePgnPanel({ orientation });
-
-    // Make the last move
-    window.setTimeout(() => {
-      const move = game.move(lastMove);
-      board.position(game.fen());
-      removeAllHighlights(boardElement); // Remove any highlights
-      highlightChecks(game, boardElement); // Highlight any new checks
-      highlightSquare(boardElement, move.from, 'move-highlight'); // Highlight the "from" move
-      highlightSquare(boardElement, move.to, 'move-highlight'); // Highlight the "to" move
-    }, 400);
-  });
-};
-
 const sliders = document.querySelectorAll('.slider > input');
 const displayElement = document.querySelector('.rating-range-values');
 if (sliders && displayElement) {
@@ -380,5 +229,5 @@ setUpSampleBoards(sampleBoards);
 const positionBoards = document.querySelectorAll('.position-board');
 setUpPositionBoards(positionBoards);
 
-const puzzleBoards = document.querySelectorAll('.puzzle-board');
-setUpPuzzleBoards(puzzleBoards);
+const puzzleBoards = document.querySelector('.puzzle-board');
+setUpPuzzleBoard(puzzleBoards);
