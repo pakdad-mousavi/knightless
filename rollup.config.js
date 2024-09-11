a0e4f1f4c3d666c6434dbb07fad7c41c69277e4c
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-09-11 21:53:36 +0800

    create and move minified css file.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,26 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import fs from 'fs';
+import path from 'path';
+
+const moveCss = {
+  name: 'move-css',
+  writeBundle() {
+    const srcPath = path.resolve('assets/js/core.min.css');
+    const destDir = path.resolve('assets', 'css');
+    const destPath = path.join(destDir, 'core.min.css');
+
+    // Create the destination directory if it doesn't exist
+    if (!fs.existsSync(destDir)) {
+      fs.mkdirSync(destDir, { recursive: true });
+    }
+
+    // Move the CSS file
+    fs.renameSync(srcPath, destPath);
+    console.log('CSS moved to dist/css/bundle.css');
+  },
+};
 
 export default {
   // Input file
@@ -13,8 +34,15 @@ export default {
 
   // Watch options
   watch: {
-    include: './assets/js/**/*.{js,mjs}',
+    include: ['./assets/js/**/*.{js,mjs}', './assets/css/**/*.css'],
   },
 
-  plugins: [nodeResolve(), terser()],
+  plugins: [
+    nodeResolve(),
+    terser(),
+    postcss({
+      extract: 'core.min.css',
+    }),
+    moveCss,
+  ],
 };
