61264cfb1c3020fea885b7b3f2795ff6148cd50b
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-10-21 14:23:58 +0800

    Re-design the main details section.

--- views/helpers.js
+++ views/helpers.js
@@ -44,6 +44,37 @@ const helper = {
     return Math.floor(index / 2);
   },
 
+  getRatingRange(rating) {
+    switch (true) {
+      case rating >= 2850 && rating <= 2900:
+        return '2850 - 2900';
+
+      case rating >= 2800 && rating < 2850:
+        return '2800 - 2850';
+
+      case rating >= 2750 && rating < 2800:
+        return '2750 - 2800';
+
+      case rating >= 2700 && rating < 2750:
+        return '2700 - 2750';
+
+      case rating >= 2650 && rating < 2700:
+        return '2650 - 2700';
+
+      case rating >= 2600 && rating < 2650:
+        return '2600 - 2650';
+
+      case rating >= 2550 && rating < 2600:
+        return '2550 - 2600';
+
+      case rating >= 2500 && rating < 2550:
+        return '2500 - 2550';
+
+      default:
+        return '2500 - 2900';
+    }
+  },
+
   eq(value, comparee) {
     if (typeof value === 'string') {
       value = value.trim();
