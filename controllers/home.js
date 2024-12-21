bad8e2c077161aa52fae0376c19fae4b19051be4
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2024-12-21 15:39:41 +0800

    Update home controller.

--- controllers/home.js
+++ controllers/home.js
@@ -2,12 +2,16 @@ import { Puzzle } from '../models/puzzle.js';
 
 export const index = async (req, res) => {
   const puzzles = await Puzzle.aggregate([{ $sample: { size: 6 } }]);
-  const formattedPuzzles = puzzles.map((x) => x.moves = x.moves.split(" ")[0]);
+  const formattedPuzzles = puzzles.map((x) => {
+    x.moves = x.moves.split(' ')[0];
+    return x;
+  });
 
   const model = {
     title: 'Chessless',
     pageCategory: 'home',
-    puzzles,
+    puzzles: formattedPuzzles,
+    user: req.session.passport ? req.session.passport.user : null,
   };
   res.render('home', model);
 };
